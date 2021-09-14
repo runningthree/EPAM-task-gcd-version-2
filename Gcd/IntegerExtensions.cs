94217107e916d220f47c8488fa73afbaa4e4413f
eugene_lavyshko <tyronderden@gmail.com>
2021-09-15 01:28:50 +0300

    commit changes

--- Gcd/IntegerExtensions.cs
+++ Gcd/IntegerExtensions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Gcd
 {
@@ -17,7 +17,26 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
         public static int GetGcdByEuclidean(int a, int b)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.", nameof(a));
+            }
+
+            if (a == int.MinValue || b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"Number cannot be {int.MinValue}.");
+            }
+
+            int foo;
+            while (a != 0)
+            {
+                foo = a;
+                a = b % a;
+                b = foo;
+            }
+
+            b = Math.Abs(b);
+            return b;
         }
 
         /// <summary>
@@ -31,7 +50,18 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByEuclidean(int a, int b, int c)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (a == 0 && b == 0 && c == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.", nameof(a));
+            }
+
+            if (a == int.MinValue || b == int.MinValue || c == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"Number cannot be {int.MinValue}.");
+            }
+
+            return (a == 0 && b == 0) ? GetGcdByEuclidean(a, GetGcdByEuclidean(b, c))
+                : GetGcdByEuclidean(b, GetGcdByEuclidean(a, c));
         }
 
         /// <summary>
@@ -45,7 +75,38 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByEuclidean(int a, int b, params int[] other)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            int lengthOfParams, result;
+            lengthOfParams = other.Length;
+            for (int i = 0; i < other.Length; i++)
+            {
+                if (other[i] == 0)
+                {
+                    lengthOfParams--;
+                }
+
+                if (other[i] == int.MinValue)
+                {
+                    lengthOfParams = -1;
+                }
+            }
+
+            if (a == 0 && b == 0 && lengthOfParams == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.", nameof(a));
+            }
+
+            if (a == int.MinValue || b == int.MinValue || lengthOfParams == -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"Number cannot be {int.MinValue}.");
+            }
+
+            result = (a == 0 && b == 0) ? 0 : GetGcdByEuclidean(a, b);
+            foreach (int item in other)
+            {
+                result = (result == 0 && item == 0) ? 0 : GetGcdByEuclidean(result, item);
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -58,7 +119,58 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
         public static int GetGcdByStein(int a, int b)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.", nameof(a));
+            }
+
+            if (a == int.MinValue || b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"Number cannot be {int.MinValue}.");
+            }
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
+
+            int k;
+            for (k = 0; ((a | b) & 1) == 0; ++k)
+            {
+                a >>= 1;
+                b >>= 1;
+            }
+
+            while ((a & 1) == 0)
+            {
+                a >>= 1;
+            }
+
+            do
+            {
+                while ((b & 1) == 0)
+                {
+                    b >>= 1;
+                }
+
+                if (a > b)
+                {
+                    int temp = a;
+                    a = b;
+                    b = temp;
+                }
+
+                b -= a;
+            }
+            while (b != 0);
+            return a << k;
         }
 
         /// <summary>
@@ -72,7 +184,18 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByStein(int a, int b, int c)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (a == 0 && b == 0 && c == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.", nameof(a));
+            }
+
+            if (a == int.MinValue || b == int.MinValue || c == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"Number cannot be {int.MinValue}.");
+            }
+
+            return (a == 0 && b == 0) ? GetGcdByStein(a, GetGcdByStein(b, c))
+                : GetGcdByStein(b, GetGcdByStein(a, c));
         }
 
         /// <summary>
@@ -86,7 +209,38 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByStein(int a, int b, params int[] other)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            int lengthOfParams, result;
+            lengthOfParams = other.Length;
+            for (int i = 0; i < other.Length; i++)
+            {
+                if (other[i] == 0)
+                {
+                    lengthOfParams--;
+                }
+
+                if (other[i] == int.MinValue)
+                {
+                    lengthOfParams = -1;
+                }
+            }
+
+            if (a == 0 && b == 0 && lengthOfParams == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.", nameof(a));
+            }
+
+            if (a == int.MinValue || b == int.MinValue || lengthOfParams == -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"Number cannot be {int.MinValue}.");
+            }
+
+            result = (a == 0 && b == 0) ? 0 : GetGcdByStein(a, b);
+            foreach (int item in other)
+            {
+                result = (result == 0 && item == 0) ? 0 : GetGcdByStein(result, item);
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -100,7 +254,11 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
         public static int GetGcdByEuclidean(out long elapsedTicks, int a, int b)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            System.Diagnostics.Stopwatch time = System.Diagnostics.Stopwatch.StartNew();
+            int result = GetGcdByEuclidean(a, b);
+            time.Stop();
+            elapsedTicks = time.ElapsedTicks;
+            return result;
         }
 
         /// <summary>
@@ -115,7 +273,11 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByEuclidean(out long elapsedTicks, int a, int b, int c)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            System.Diagnostics.Stopwatch time = System.Diagnostics.Stopwatch.StartNew();
+            int result = GetGcdByEuclidean(a, b, c);
+            time.Stop();
+            elapsedTicks = time.ElapsedTicks;
+            return result;
         }
 
         /// <summary>
@@ -130,7 +292,11 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByEuclidean(out long elapsedTicks, int a, int b, params int[] other)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            System.Diagnostics.Stopwatch time = System.Diagnostics.Stopwatch.StartNew();
+            int result = GetGcdByEuclidean(a, b, other);
+            time.Stop();
+            elapsedTicks = time.ElapsedTicks;
+            return result;
         }
 
         /// <summary>
@@ -144,7 +310,11 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
         public static int GetGcdByStein(out long elapsedTicks, int a, int b)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            System.Diagnostics.Stopwatch time = System.Diagnostics.Stopwatch.StartNew();
+            int result = GetGcdByStein(a, b);
+            time.Stop();
+            elapsedTicks = time.ElapsedTicks;
+            return result;
         }
 
         /// <summary>
@@ -159,7 +329,11 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByStein(out long elapsedTicks, int a, int b, int c)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            System.Diagnostics.Stopwatch time = System.Diagnostics.Stopwatch.StartNew();
+            int result = GetGcdByStein(a, b, c);
+            time.Stop();
+            elapsedTicks = time.ElapsedTicks;
+            return result;
         }
 
         /// <summary>
@@ -174,7 +348,11 @@ namespace Gcd
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
         public static int GetGcdByStein(out long elapsedTicks, int a, int b, params int[] other)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            System.Diagnostics.Stopwatch time = System.Diagnostics.Stopwatch.StartNew();
+            int result = GetGcdByStein(a, b, other);
+            time.Stop();
+            elapsedTicks = time.ElapsedTicks;
+            return result;
         }
     }
 }
